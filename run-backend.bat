060a8c103b35906103f8056b3a0fe045aec1e0a7
soikcha <ckthdlr@naver.com>
2023-03-17 02:32:43 +0900

    bat에서 yarn 명령어 앞에 call 추가

--- run-backend.bat
+++ run-backend.bat
@@ -1,5 +1,26 @@
 @echo off
 
+goto :run
+
+:start
+cd ./backend
+call yarn start
+cd ..
+exit /b 0
+
+:install
+cd ./backend
+call yarn install
+cd ..
+exit /b 0
+
+:build
+cd ./backend
+call yarn install --immutable --immutable-cache --check-cache
+call yarn build
+cd ..
+exit /b 0
+
 :run
 IF "%~1" == "start" (
   call :start
@@ -7,23 +28,8 @@ IF "%~1" == "start" (
   call :install
 ) ELSE IF "%~1" == "build" (
   call :build
+) ELSE IF "%~1" == "" (
+  echo "명령어를 입력하세요: start, install, build"
+) ELSE (
+  echo "잘못된 명령어입니다. 다음 중 하나를 입력하세요: start, install, build"
 )
-
-:start
-cd ./backend
-yarn start
-cd ..
-exit /b 0
-
-:install
-cd ./backend
-yarn install
-cd ..
-exit /b 0
-
-:build
-cd ./backend
-yarn install --immutable --immutable-cache --check-cache
-yarn build
-cd ..
-exit /b 0
--- run-frontend.bat
+++ run-frontend.bat
@@ -1,5 +1,26 @@
 @echo off
 
+goto :run
+
+:start
+cd ./frontend
+call yarn start
+cd ..
+exit /b 0
+
+:install
+cd ./frontend
+call yarn install
+cd ..
+exit /b 0
+
+:build
+cd ./frontend
+call yarn install --immutable --immutable-cache --check-cache
+call yarn build
+cd ..
+exit /b 0
+
 :run
 IF "%~1" == "start" (
   call :start
@@ -7,23 +28,8 @@ IF "%~1" == "start" (
   call :install
 ) ELSE IF "%~1" == "build" (
   call :build
+) ELSE IF "%~1" == "" (
+  echo "명령어를 입력하세요: start, install, build"
+) ELSE (
+  echo "잘못된 명령어입니다. 다음 중 하나를 입력하세요: start, install, build"
 )
-
-:start
-cd ./frontend
-yarn start
-cd ..
-exit /b 0
-
-:install
-cd ./frontend
-yarn install
-cd ..
-exit /b 0
-
-:build
-cd ./frontend
-yarn install --immutable --immutable-cache --check-cache
-yarn build
-cd ..
-exit /b 0
